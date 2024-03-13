71c0ce99fd6bf572eed73c8b3eb50af7c135464d
Steven Wu <steven@Stevens-Air.local>
2024-03-13 00:19:13 -0500

    init

---
+++ app/Header/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+	return (
+		<div className='header'>
+			<div className='container'>
+				<img src="/Log_It_Logo.png" alt="" />
+			</div>
+
+			<div className='container'>
+				<input className='search' placeholder="Wendy's nuts hit your chin."/>
+			</div>
+
+		</div>
+  	)
+}
